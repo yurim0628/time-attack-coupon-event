d512b1b62b25aa52eb83d62012c51435e275fccd
yurim0628 <dbfla0303@naver.com>
2024-11-10 15:52:14 +0900

    chore: Multi Module Project 구성 - Issue-Coupon Module

---
+++ issue-coupon/src/main/java/org/example/issuecoupon/IssueCouponApplication.java
@@ -0,0 +1,13 @@
+package org.example.issuecoupon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IssueCouponApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IssueCouponApplication.class, args);
+    }
+
+}
---
+++ issue-coupon/src/test/java/org/example/issuecoupon/IssueCouponApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.issuecoupon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IssueCouponApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
