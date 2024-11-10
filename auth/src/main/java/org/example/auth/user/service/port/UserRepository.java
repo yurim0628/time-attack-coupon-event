72df90c178ec73dbca93b2c4d85e02fb97ac0fd9
yurim0628 <dbfla0303@naver.com>
2024-11-10 18:51:56 +0900

    feat: findByEmail 메서드 추가하여 이메일로 사용자 조회 기능 구현

--- auth/src/main/java/org/example/auth/user/service/port/UserRepository.java
+++ auth/src/main/java/org/example/auth/user/service/port/UserRepository.java
@@ -2,9 +2,13 @@ package org.example.auth.user.service.port;
 
 import org.example.auth.user.domain.User;
 
+import java.util.Optional;
+
 public interface UserRepository {
 
     User save(User user);
 
     boolean existsByEmail(String email);
+
+    Optional<User> findByEmail(String email);
 }
