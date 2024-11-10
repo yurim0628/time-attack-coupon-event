ed1e39a32a62c1f679e3b4621200f0703c163e7f
yurim0628 <dbfla0303@naver.com>
2024-11-10 23:29:49 +0900

    feat: CouponRepositoryImpl 추가하여 쿠폰 저장 및 조회 기능 구현(CouponJpaRepository와 연동)

---
+++ coupon/src/main/java/org/example/coupon/infrastructure/CouponRepositoryImpl.java
@@ -0,0 +1,26 @@
+package org.example.coupon.infrastructure;
+
+import lombok.RequiredArgsConstructor;
+import org.example.coupon.domain.Coupon;
+import org.example.coupon.infrastructure.entity.CouponEntity;
+import org.example.coupon.service.port.CouponRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+@RequiredArgsConstructor
+public class CouponRepositoryImpl implements CouponRepository {
+
+    private final CouponJpaRepository couponJpaRepository;
+
+    @Override
+    public Optional<Coupon> findById(Long id) {
+        return couponJpaRepository.findById(id).map(CouponEntity::toModel);
+    }
+
+    @Override
+    public Coupon save(Coupon coupon) {
+        return couponJpaRepository.save(CouponEntity.fromModel(coupon)).toModel();
+    }
+}
