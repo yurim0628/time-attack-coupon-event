f1dfcc733b6b2d10e61d04a77150ad1b14a39175
yurim0628 <dbfla0303@naver.com>
2024-11-10 23:36:29 +0900

    feat: CouponIssueController 추가하여 쿠폰 발급 API 구현

---
+++ issue-coupon/src/main/java/org/example/issuecoupon/controller/CouponIssueController.java
@@ -0,0 +1,22 @@
+package org.example.issuecoupon.controller;
+
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.example.issuecoupon.domain.SaveCouponIssueRequest;
+import org.example.issuecoupon.service.CouponIssueService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/coupons")
+@RequiredArgsConstructor
+public class CouponIssueController {
+
+    private final CouponIssueService couponIssueService;
+
+    @PostMapping("/issues")
+    public ResponseEntity<Void> issueCoupon(@RequestBody @Valid SaveCouponIssueRequest saveCouponIssueRequest) {
+        couponIssueService.issueCoupon(saveCouponIssueRequest);
+        return ResponseEntity.ok().build();
+    }
+}
