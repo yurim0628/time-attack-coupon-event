0be44895c96decab3416e9280832d605b6f2a741
yurim0628 <dbfla0303@naver.com>
2024-11-10 23:28:10 +0900

    feat: CouponRepository 인터페이스 추가하여 쿠폰 저장 및 조회 기능 정의

---
+++ coupon/src/main/java/org/example/coupon/service/port/CouponRepository.java
@@ -0,0 +1,12 @@
+package org.example.coupon.service.port;
+
+import org.example.coupon.domain.Coupon;
+
+import java.util.Optional;
+
+public interface CouponRepository {
+
+    Optional<Coupon> findById(Long id);
+
+    Coupon save(Coupon coupon);
+}
