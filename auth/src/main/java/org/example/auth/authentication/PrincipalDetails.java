6ee1000fa1f077dfc11d8d270e11888ba68f7cab
yurim0628 <dbfla0303@naver.com>
2024-11-10 18:47:26 +0900

    feat: PrincipalDetails 및 PrincipalDetailsService 구현하여 사용자 인증 및 권한 정보 제공

---
+++ auth/src/main/java/org/example/auth/authentication/PrincipalDetails.java
@@ -0,0 +1,46 @@
+package org.example.auth.authentication;
+
+import org.example.auth.user.domain.User;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.Collections;
+
+public record PrincipalDetails(User user) implements UserDetails {
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return Collections.singleton((GrantedAuthority) () -> user.getAuthority().toString());
+    }
+
+    @Override
+    public String getPassword() {
+        return user.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return user.getEmail();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
