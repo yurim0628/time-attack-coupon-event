3189418c2538ba57029ddc0ce00c63844b063d04
yurim0628 <dbfla0303@naver.com>
2024-11-10 16:02:56 +0900

    feat: 사용자 정의 예외 및 글로벌 예외 처리기 추가

---
+++ auth/src/main/java/org/example/auth/common/exception/UserException.java
@@ -0,0 +1,19 @@
+package org.example.auth.common.exception;
+
+import lombok.Getter;
+
+@Getter
+public class UserException extends RuntimeException {
+
+    private final ErrorCode errorCode;
+
+    public UserException(String message, ErrorCode errorCode) {
+        super(message);
+        this.errorCode = errorCode;
+    }
+
+    public UserException(ErrorCode errorCode) {
+        super(errorCode.getMessage());
+        this.errorCode = errorCode;
+    }
+}
