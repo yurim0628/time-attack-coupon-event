ba5027dd57daeec42169e3e641d4a1785d1178ab
yurim0628 <dbfla0303@naver.com>
2024-11-11 18:13:21 +0900

    feat: CouponJpaRepository에 updateIssuedQuantity 메소드 추가

--- coupon/src/main/java/org/example/coupon/infrastructure/CouponJpaRepository.java
+++ coupon/src/main/java/org/example/coupon/infrastructure/CouponJpaRepository.java
@@ -2,6 +2,20 @@ package org.example.coupon.infrastructure;
 
 import org.example.coupon.infrastructure.entity.CouponEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface CouponJpaRepository extends JpaRepository<CouponEntity, Long> {
+
+    @Modifying
+    @Query("""
+           UPDATE CouponEntity c
+           SET c.issuedQuantity = :issuedQuantity
+           WHERE c.id = :id
+           """)
+    void updateIssuedQuantity(
+            @Param("id") Long id,
+            @Param("issuedQuantity") Long issuedQuantity
+    );
 }
