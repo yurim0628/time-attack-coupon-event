765575532dc960d2401506e61df8b0e491bf542c
yurim0628 <dbfla0303@naver.com>
2024-11-11 15:27:02 +0900

    refactor: CouponIssueCacheStore 메서드 변경 - 기존 메서드 제거하고, checkCouponIssueAvailability 추가

--- redis/src/main/java/org/example/redis/infrastructure/CouponIssueCacheStoreImpl.java
+++ redis/src/main/java/org/example/redis/infrastructure/CouponIssueCacheStoreImpl.java
@@ -11,12 +11,7 @@ public class CouponIssueCacheStoreImpl implements CouponIssueCacheStore {
     private final CouponIssueRedisCache couponIssueRedisCache;
 
     @Override
-    public Long getIssuedCouponUserCount(String issueRequestKey) {
-        return couponIssueRedisCache.getIssuedCouponUserCount(issueRequestKey);
-    }
-
-    @Override
-    public Long addIssuedCouponUser(String issueRequestKey, String userId) {
-        return couponIssueRedisCache.addIssuedCouponUser(issueRequestKey, userId);
+    public Long checkCouponIssueAvailability(String couponIssueRequestKey, String maxQuantity, String userId) {
+        return couponIssueRedisCache.checkCouponIssueAvailability(couponIssueRequestKey, maxQuantity, userId);
     }
 }
