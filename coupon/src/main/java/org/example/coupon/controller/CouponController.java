6321272a1bbc35d0ae9b75bd7cdd0206c9a7ad91
yurim0628 <dbfla0303@naver.com>
2024-11-13 18:23:14 +0900

    feat: CouponController에 쿠폰 조회 API 구현

---
+++ coupon/src/main/java/org/example/coupon/controller/CouponController.java
@@ -0,0 +1,24 @@
+package org.example.coupon.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.example.coupon.domain.GetCouponResponse;
+import org.example.coupon.service.CouponService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/coupons")
+@RequiredArgsConstructor
+
+public class CouponController {
+
+    private final CouponService couponService;
+
+    @GetMapping("/{couponId}")
+    public ResponseEntity<GetCouponResponse> getCoupon(@PathVariable Long couponId) {
+        return ResponseEntity.ok(couponService.getCoupon(couponId));
+    }
+}
