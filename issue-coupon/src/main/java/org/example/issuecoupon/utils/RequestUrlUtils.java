1388181ecae127c84e684f037bfc2a3cd58402e7
yurim0628 <dbfla0303@naver.com>
2024-11-13 18:24:46 +0900

    feat: RequestUrlUtils 유틸 클래스 추가 및 URI 생성 메서드 구현

---
+++ issue-coupon/src/main/java/org/example/issuecoupon/utils/RequestUrlUtils.java
@@ -0,0 +1,35 @@
+package org.example.issuecoupon.utils;
+
+import lombok.experimental.UtilityClass;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import static java.nio.charset.StandardCharsets.UTF_8;
+
+@UtilityClass
+public class RequestUrlUtils {
+
+    public static final String COUPON_SERVICE_URL = "http://localhost:8082";
+    public static final String REDIS_SERVICE_URL = "http://localhost:8084";
+
+    public static final String GET_COUPONS_FROM_DB_ENDPOINT = "/coupons/{couponId}";
+    public static final String GET_COUPONS_FROM_CACHE_ENDPOINT = "/redis/coupons/{couponId}";
+    public static final String SAVE_COUPONS_FROM_CACHE_ENDPOINT = "/redis/coupons";
+    public static final String VALIDATE_ISSUE_COUPONS_ENDPOINT = "/redis/coupon/issues/validate";
+
+    public static String buildUri(String baseUrl, String endpoint) {
+        return UriComponentsBuilder
+                .fromUriString(baseUrl)
+                .path(endpoint)
+                .encode(UTF_8)
+                .toUriString();
+    }
+
+    public static String buildUriWithPathVariable(String baseUrl, String endpoint, Long pathVariable) {
+        return UriComponentsBuilder
+                .fromUriString(baseUrl)
+                .path(endpoint)
+                .buildAndExpand(pathVariable)
+                .encode(UTF_8)
+                .toUriString();
+    }
+}
