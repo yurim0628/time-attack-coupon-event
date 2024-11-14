1798e8ef9433d7c94fbb324d2322fe1b9b656346
yurim0628 <dbfla0303@naver.com>
2024-11-14 16:37:31 +0900

    chore: 스케줄링 설정을 활성화하기 위해 @Configuration 어노테이션 추가

--- coupon/src/main/java/org/example/coupon/config/ScheduleConfig.java
+++ coupon/src/main/java/org/example/coupon/config/ScheduleConfig.java
@@ -1,7 +1,9 @@
 package org.example.coupon.config;
 
+import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
+@Configuration
 @EnableScheduling
 public class ScheduleConfig {
 }
