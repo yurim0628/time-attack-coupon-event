eee59113474d5584747bd30f6f640aacc64b8f2c
yurim0628 <dbfla0303@naver.com>
2024-11-18 17:09:48 +0900

    chore: CouponService 클래스 패키지 이동

--- coupon/src/main/java/org/example/coupon/controller/CouponController.java
+++ coupon/src/main/java/org/example/coupon/controller/CouponController.java
@@ -2,7 +2,7 @@ package org.example.coupon.controller;
 
 import lombok.RequiredArgsConstructor;
 import org.example.coupon.domain.GetCouponResponse;
-import org.example.coupon.service.CouponService;
+import org.example.coupon.service.business.CouponService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
