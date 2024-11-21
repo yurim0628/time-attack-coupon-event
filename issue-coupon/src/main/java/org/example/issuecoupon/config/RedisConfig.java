edb262549b764daccd0f0acb22371aab1e11f4e4
yurim0628 <dbfla0303@naver.com>
2024-11-21 15:51:48 +0900

    feat: Redis Sentinel 설정 추가

--- issue-coupon/src/main/java/org/example/issuecoupon/config/RedisConfig.java
+++ issue-coupon/src/main/java/org/example/issuecoupon/config/RedisConfig.java
@@ -1,8 +1,9 @@
 package org.example.issuecoupon.config;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisSentinelConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
@@ -10,15 +11,20 @@ import org.springframework.data.redis.serializer.StringRedisSerializer;
 @Configuration
 public class RedisConfig {
 
-    @Value("${spring.data.redis.host}")
-    private String host;
-
-    @Value("${spring.data.redis.port}")
-    private int port;
+    private static final String MASTER_NAME = "my-master";
+    private static final String SENTINEL_HOST = "localhost";
+    private static final int SENTINEL_PORT_1 = 5000;
+    private static final int SENTINEL_PORT_2 = 5001;
+    private static final int SENTINEL_PORT_3 = 5002;
 
     @Bean
-    public LettuceConnectionFactory redisConnectionFactory() {
-        return new LettuceConnectionFactory(host, port);
+    public RedisConnectionFactory redisConnectionFactory() {
+        RedisSentinelConfiguration redisSentinelConfiguration = new RedisSentinelConfiguration()
+                .master(MASTER_NAME)
+                .sentinel(SENTINEL_HOST, SENTINEL_PORT_1)
+                .sentinel(SENTINEL_HOST, SENTINEL_PORT_2)
+                .sentinel(SENTINEL_HOST, SENTINEL_PORT_3);
+        return new LettuceConnectionFactory(redisSentinelConfiguration);
     }
 
     @Bean
