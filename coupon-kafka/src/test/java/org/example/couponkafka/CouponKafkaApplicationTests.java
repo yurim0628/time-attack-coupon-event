e19415bc91eb2b3489cb11f3d2fbed3aa16627b8
yurim0628 <dbfla0303@naver.com>
2024-11-23 16:56:36 +0900

    Create Kafka Module

---
+++ coupon-kafka/src/test/java/org/example/couponkafka/CouponKafkaApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.couponkafka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CouponKafkaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
