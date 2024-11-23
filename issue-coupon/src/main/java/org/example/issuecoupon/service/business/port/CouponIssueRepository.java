791ee0dcb3c0bc81639528c0b07a3b77737a4df5
yurim0628 <dbfla0303@naver.com>
2024-11-23 16:58:16 +0900

    chore: 쿠폰 발급 서비스 분리하면서 불필요해진 클래스 제거

--- issue-coupon/src/main/java/org/example/issuecoupon/service/business/port/CouponIssueRepository.java
+++
@@ -1,8 +0,0 @@
-package org.example.issuecoupon.service.business.port;
-
-import org.example.issuecoupon.domain.CouponIssue;
-
-public interface CouponIssueRepository {
-
-    void save(CouponIssue couponIssue);
-}
