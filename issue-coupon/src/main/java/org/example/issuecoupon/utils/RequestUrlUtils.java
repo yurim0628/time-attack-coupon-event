f0cfef198aa26d11d22b1b79019c22c2f2cd300c
yurim0628 <dbfla0303@naver.com>
2024-11-25 04:50:33 +0900

    refactor: 쿠폰 서비스 URL 및 엔드포인트를 @Value를 사용하여 yml에서 가져오도록 변경

--- issue-coupon/src/main/java/org/example/issuecoupon/utils/RequestUrlUtils.java
+++ issue-coupon/src/main/java/org/example/issuecoupon/utils/RequestUrlUtils.java
@@ -8,9 +8,6 @@ import static java.nio.charset.StandardCharsets.UTF_8;
 @UtilityClass
 public class RequestUrlUtils {
 
-    public static final String COUPON_SERVICE_URL = "http://localhost:8082";
-    public static final String GET_COUPONS_FROM_DB_ENDPOINT = "/coupons/{couponId}";
-
     public static String buildUriWithPathVariable(String baseUrl, String endpoint, Long pathVariable) {
         return UriComponentsBuilder
                 .fromUriString(baseUrl)
