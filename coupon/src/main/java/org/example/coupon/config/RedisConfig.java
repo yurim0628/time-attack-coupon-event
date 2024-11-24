41795734556bf105c3a5038ef1989304f0719600
yurim0628 <dbfla0303@naver.com>
2024-11-25 04:49:56 +0900

    refactor: SpringRedisAutoConfiguration으로 Redis Sentinel 설정 리팩토링

--- coupon/src/main/java/org/example/coupon/config/RedisConfig.java
+++ coupon/src/main/java/org/example/coupon/config/RedisConfig.java
@@ -3,28 +3,16 @@ package org.example.coupon.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
-import org.springframework.data.redis.connection.RedisSentinelConfiguration;
-import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
 public class RedisConfig {
 
-    private static final String MASTER_NAME = "my-master";
-    private static final String SENTINEL_HOST = "localhost";
-    private static final int SENTINEL_PORT_1 = 5000;
-    private static final int SENTINEL_PORT_2 = 5001;
-    private static final int SENTINEL_PORT_3 = 5002;
+    private final RedisConnectionFactory redisConnectionFactory;
 
-    @Bean
-    public RedisConnectionFactory redisConnectionFactory() {
-        RedisSentinelConfiguration redisSentinelConfiguration = new RedisSentinelConfiguration()
-                .master(MASTER_NAME)
-                .sentinel(SENTINEL_HOST, SENTINEL_PORT_1)
-                .sentinel(SENTINEL_HOST, SENTINEL_PORT_2)
-                .sentinel(SENTINEL_HOST, SENTINEL_PORT_3);
-        return new LettuceConnectionFactory(redisSentinelConfiguration);
+    public RedisConfig(RedisConnectionFactory redisConnectionFactory) {
+        this.redisConnectionFactory = redisConnectionFactory;
     }
 
     @Bean
@@ -32,7 +20,7 @@ public class RedisConfig {
         RedisTemplate<String, String> redisTemplate = new RedisTemplate<>();
         redisTemplate.setKeySerializer(new StringRedisSerializer());
         redisTemplate.setValueSerializer(new StringRedisSerializer());
-        redisTemplate.setConnectionFactory(redisConnectionFactory());
+        redisTemplate.setConnectionFactory(redisConnectionFactory);
         return redisTemplate;
     }
 }
