0c3391ea35b589c880c873ddb1cde2187b25995e
yurim0628 <dbfla0303@naver.com>
2024-11-27 06:34:50 +0900

    feat: KafkaProducerConfig에 batch-size, linger-ms, buffer-memory 설정 추가

--- issue-coupon/src/main/java/org/example/issuecoupon/config/KafkaProducerConfig.java
+++ issue-coupon/src/main/java/org/example/issuecoupon/config/KafkaProducerConfig.java
@@ -20,12 +20,26 @@ public class KafkaProducerConfig {
     @Value("${spring.kafka.bootstrap-servers}")
     private String bootstrapServers;
 
+    @Value("${spring.kafka.producer.batch-size}")
+    private int batchSize;
+
+    @Value("${spring.kafka.producer.linger-ms}")
+    private int lingerMs;
+
+    @Value("${spring.kafka.producer.buffer-memory}")
+    private int bufferMemory;
+
     @Bean
     public ProducerFactory<String, Object> producerFactory() {
         Map<String, Object> producerConfig = new HashMap<>();
         producerConfig.put(BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
         producerConfig.put(KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
         producerConfig.put(VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
+
+        producerConfig.put(BATCH_SIZE_CONFIG, batchSize);
+        producerConfig.put(LINGER_MS_CONFIG, lingerMs);
+        producerConfig.put(BUFFER_MEMORY_CONFIG, bufferMemory);
+
         return new DefaultKafkaProducerFactory<>(producerConfig);
     }
 
