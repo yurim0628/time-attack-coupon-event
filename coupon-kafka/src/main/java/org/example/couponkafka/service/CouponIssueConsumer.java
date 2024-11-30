4102686b64ad2caabf142c96fd3a894203fcf39a
yurim0628 <dbfla0303@naver.com>
2024-11-30 19:17:29 +0900

    feat: Kafka Consumer에 수동 오프셋 커밋을 위한 Acknowledgment 추가

--- coupon-kafka/src/main/java/org/example/couponkafka/service/CouponIssueConsumer.java
+++ coupon-kafka/src/main/java/org/example/couponkafka/service/CouponIssueConsumer.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import org.example.couponkafka.domain.CouponIssue;
 import org.example.couponkafka.service.port.CouponIssueRepository;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.support.Acknowledgment;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -12,9 +13,13 @@ public class CouponIssueConsumer {
 
     private final CouponIssueRepository couponIssueRepository;
 
-    @KafkaListener(topics = "TimeAttackCouponIssue", groupId = "Coupon-TimeAttackCouponIssue")
-    public void listener(CouponIssue couponIssue) {
+    @KafkaListener(
+            topics = "TimeAttackCouponIssue",
+            groupId = "Coupon-TimeAttackCouponIssue"
+    )
+    public void listener(CouponIssue couponIssue, Acknowledgment acknowledgment) {
         saveCouponIssue(couponIssue);
+        acknowledgment.acknowledge();
     }
 
     private void saveCouponIssue(CouponIssue couponIssue) {
