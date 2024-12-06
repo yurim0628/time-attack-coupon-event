f75088b8d8c1c76ba237a5d6b19090f56cabb932
yurim0628 <dbfla0303@naver.com>
2024-12-07 04:33:27 +0900

    refactor: KafkaTemplate 타입 수정

--- coupon-kafka/src/main/java/org/example/couponkafka/service/DltMessageHandler.java
+++ coupon-kafka/src/main/java/org/example/couponkafka/service/DltMessageHandler.java
@@ -1,29 +1,22 @@
 package org.example.couponkafka.service;
 
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.example.couponkafka.domain.CouponIssue;
 import org.springframework.kafka.core.KafkaTemplate;
-import org.springframework.messaging.handler.annotation.Header;
-import org.springframework.stereotype.Service;
+import org.springframework.stereotype.Component;
 
-import static org.springframework.kafka.support.KafkaHeaders.RECEIVED_TOPIC;
-
-@Service
 @Slf4j
+@Component
+@RequiredArgsConstructor
 public class DltMessageHandler {
 
-    private static final String ORIGINAL_TOPIC = "TimeAttackCouponIssue";
-    private final KafkaTemplate<String, CouponIssue> kafkaTemplate;
+    private final KafkaTemplate<String, Object> kafkaTemplate;
 
-    public DltMessageHandler(KafkaTemplate<String, CouponIssue> kafkaTemplate) {
-        this.kafkaTemplate = kafkaTemplate;
-    }
-
-    public void handleDltMessage(ConsumerRecord<String, CouponIssue> record,
-                                 @Header(RECEIVED_TOPIC) String topic) {
-        log.error("Received Message in DLT. Topic: [{}], Value: [{}]", topic, record.value());
-        kafkaTemplate.send(ORIGINAL_TOPIC, record.key(), record.value());
-        log.info("Message Successfully Reprocessed. Value: [{}]", record.value());
+    public void handleDltMessage(ConsumerRecord<String, CouponIssue> record) {
+        log.info("Starting DLT Message Processing. Consumer Record: [{}]", record);
+        kafkaTemplate.send("TimeAttackCouponIssue", record.value());
+        log.info("Completed DLT Message Processing. CouponIssue Sent: [{}]", record.value());
     }
 }
