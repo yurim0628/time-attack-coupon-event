667148d7ca02ca9479c89757f6863a04ec3e4f7e
yurim0628 <dbfla0303@naver.com>
2025-08-04 03:35:05 +0900

    feat: Kafka 프로듀서 설정에 Micrometer 관찰 기능 추가 및 프로듀서 옵션 상세화

--- issue-coupon/src/main/java/org/example/issuecoupon/config/KafkaProducerConfig.java
+++ issue-coupon/src/main/java/org/example/issuecoupon/config/KafkaProducerConfig.java
@@ -7,6 +7,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.core.DefaultKafkaProducerFactory;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.core.ProducerFactory;
+import org.springframework.kafka.support.micrometer.KafkaTemplateObservation;
 import org.springframework.kafka.support.serializer.JsonSerializer;
 
 import java.util.HashMap;
@@ -70,6 +71,11 @@ public class KafkaProducerConfig {
 
     @Bean
     public KafkaTemplate<String, Object> kafkaTemplate() {
-        return new KafkaTemplate<>(producerFactory());
+        KafkaTemplate<String, Object> kafkaTemplate = new KafkaTemplate<>(producerFactory());
+        kafkaTemplate.setObservationEnabled(true);
+        kafkaTemplate.setObservationConvention(
+                KafkaTemplateObservation.DefaultKafkaTemplateObservationConvention.INSTANCE
+        );
+        return kafkaTemplate;
     }
 }
