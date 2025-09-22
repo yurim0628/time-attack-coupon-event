3b37890e855c2018f20b1e69416d739cae642d77
yurim0628 <dbfla0303@naver.com>
2025-09-22 17:17:45 +0900

    feat: CouponIssueConsumer에 트랜잭션 커밋 후 오프셋 커밋 및 중복 처리 로직 추가

--- coupon-kafka/src/main/java/org/example/couponkafka/service/CouponIssueConsumer.java
+++ coupon-kafka/src/main/java/org/example/couponkafka/service/CouponIssueConsumer.java
@@ -4,10 +4,13 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.example.couponkafka.domain.CouponIssue;
 import org.example.couponkafka.service.port.CouponIssueRepository;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.kafka.support.Acknowledgment;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import static org.example.couponkafka.util.TransactionUtils.registerAfterCommit;
 
 @Slf4j
 @Service
@@ -23,8 +26,13 @@ public class CouponIssueConsumer {
     @Transactional
     public void listener(CouponIssue couponIssue, Acknowledgment acknowledgment) {
         try {
-            log.info("Received CouponIssue for User ID: [{}]", couponIssue.getUserId());
+            log.info("Received CouponIssue: User ID: [{}], Coupon ID: [{}]",
+                    couponIssue.getUserId(), couponIssue.getCouponId());
             saveCouponIssue(couponIssue);
+            registerAfterCommit(acknowledgment::acknowledge);
+        } catch (DataIntegrityViolationException e) {
+            log.warn("Duplicate CouponIssue detected for User ID: [{}], Coupon ID: [{}]",
+                    couponIssue.getUserId(), couponIssue.getCouponId());
             acknowledgment.acknowledge();
         } catch (Exception e) {
             throw new RuntimeException("Failed to Save CouponIssue. User ID: " + couponIssue.getUserId(), e);
